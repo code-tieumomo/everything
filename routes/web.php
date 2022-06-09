7ad54d47c8fd307170018c84756a6e9dc35983bf
Vinh Vu <vinhvu5112000@gmail.com>
2022-06-09 09:13:04 +0700

    Update .gitignore

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(["middleware"=>"auth"],function (){
+    Route::get("/", function (){
+        return view('dashboard');
+    });
 });
 
 Route::get('/dashboard', function () {
