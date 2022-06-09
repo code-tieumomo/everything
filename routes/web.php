6f92c1f5f29a93c82a77aa21c8cf8232ea42b03b
code-tieumomo <code.tieumomo@gmail.com>
2022-06-09 16:23:04 +0700

    Landing page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function() {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard.index');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
